f049776b36606f945de4071fa6e98dc0c567b129
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-05-22 20:46:15 +0600

    add cors

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const order_route_1 = require("./modules/order/order.route");
 const product_route_1 = require("./modules/product/product.route");
 const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 // routers
 app.use("/api/products", product_route_1.ProductRouters);
 app.use("/api/orders", order_route_1.OrderRouters);
